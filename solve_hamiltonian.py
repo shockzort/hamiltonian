e8728131894be5b5e5dc5d65f9efb82efe78aab1
Shockzor <vbondarenko@cnord.ru>
2024-10-07 19:16:24 +0300

    second try

--- solve_hamiltonian.py
+++ solve_hamiltonian.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 import sys
 import plotly.graph_objects as go
 
-
 class discrete_point:
     x: int
     y: int
+    idx: int
 
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, idx: int):
         self.x = x
         self.y = y
+        self.idx = idx
 
     def is_neighbour(self, other: discrete_point) -> bool:
         return (abs(self.x - other.x) == 1 and abs(self.y - other.y) == 0) or (
@@ -17,23 +18,22 @@ class discrete_point:
         )
 
 
+
 class node:
     p: discrete_point
     neighbours: list[discrete_point]
-    free_neighbours: list[discrete_point]
 
     def __init__(self, p: discrete_point):
         self.p = p
         self.neighbours = []
-        self.free_neighbours = []
 
     def connected(self) -> bool:
         return len(self.neighbours) == 2
 
-    def end(self) -> bool:
-        return len(self.neighbours) == 1 and len(self.free_neighbours) == 0
-
     def connect(self, other: node) -> bool:
+        if self.p.idx == other.p.idx:
+            return False
+
         if self.connected() or other.connected():
             return False
 
@@ -41,78 +41,167 @@ class node:
             return False
 
         self.neighbours.append(other.p)
-        if other.p in self.free_neighbours:
-            self.free_neighbours.remove(other.p)
-
         other.neighbours.append(self.p)
-        if self.p in other.free_neighbours:
-            other.free_neighbours.remove(self.p)
 
         return True
 
+class hamiltonian_grid:
+
+    w: int = 0
+    h: int = 0
+    nodes: dict = {}
+
+    def __init__(self, w: int, h: int):
+        if w <= 0 or h <= 0:
+            print("cannot init hamiltonian grid: dims incorrect w={}, h={}".format(w, h))
+            return
+
+        self.w = w
+        self.h = h
+
+        idx = 0
+        for x in range(w):
+            for y in range(h):
+                self.nodes[idx] = node(discrete_point(x + 0.5, y + 0.5, idx))
+                idx += 1
+
+        self._init_grid()
+
+    def _init_grid(self):
+        for n_out in self.nodes.values():
+            for n_in in self.nodes.values():
+                if n_out.p.idx == n_in.p.idx:
+                    continue
+
+                if n_out.p.is_neighbour(n_in.p):
+                    if n_in.p not in n_out.neighbours:
+                        n_out.neighbours.append(n_in.p)
+
+                    if n_out.p not in n_in.neighbours:
+                        n_in.neighbours.append(n_out.p)
+
+
+
+    def as_adjacency_matrix(self):
+        res = [[0 for i in range(self.w)]
+                 for j in range(self.h)]
+
+        for node_idx, node in self.nodes.items():
+            row = res[int(node_idx % self.w)]
+
+            row[int(node.p.idx % self.w)] = 1
+
+            for n in node.neighbours:
+                row[int(n.idx % self.w)] = 1
+
+        return res
+
+
+    def Hamiltonian_path(self) -> list:
+
+        adj_matrix = self.as_adjacency_matrix()
+        print("adj_matrix: {}".format(adj_matrix))
+
+        num_rows = len(adj_matrix)
+
+        result_path = []
+
+        dp = [[False for i in range(1 << num_rows)]
+                    for j in range(num_rows)]
+
+        # Set all dp[i][(1 << i)] to
+        # true
+        for i in range(num_rows):
+            dp[i][1 << i] = True
+
+        # Iterate over each subset
+        # of nodes
+        for i in range(1 << num_rows):
+            for j in range(num_rows):
+
+                # If the jth nodes is included
+                # in the current subset
+                if ((i & (1 << j)) != 0):
+
+                    # Find K, neighbour of j
+                    # also present in the
+                    # current subset
+                    for k in range(num_rows):
+                        if ((i & (1 << k)) != 0 and
+                                adj_matrix[k][j] == 1 and
+                                        j != k and
+                            dp[k][i ^ (1 << j)]):
+
+                            # Update dp[j][i]
+                            # to true
+                            dp[j][i] = True
+
+                            result_path.append(k*self.w + j)
+
+                            break
+
+        # Traverse the vertices
+        for i in range(num_rows):
+            # Hamiltonian Path exists
+            if (dp[i][(1 << num_rows) - 1]):
+                return result_path
+
+        # Otherwise, return false
+        return []
+
 
 def main() -> int:
-    nodes = []
 
     w = 8
     h = 8
 
-    for x in range(w):
-        for y in range(h):
-            nodes.append(node(discrete_point(x + 0.5, y + 0.5)))
+    grid = hamiltonian_grid(w, h)
 
-    def pick_node(nodes: list[node]):
+    p = grid.Hamiltonian_path()
+
+    nodes = grid.nodes.values()
+
+    if p != []:
+        fig = go.Figure()
+
         for n in nodes:
-            if not n.connected():
-                return n
+            # Add shapes
 
-        return None
-
-    for n in nodes:
-        sel = pick_node(nodes)
-        if sel is not None:
-            n.connect(sel)
-        else:
-            print("all connected")
-            break
-
-    fig = go.Figure()
-
-    for n in nodes:
-        # Add shapes
-
-        fig.add_shape(
-            type="rect",
-            x0=(n.p.x - 0.5),
-            y0=(n.p.y - 0.5),
-            x1=(n.p.x + 0.5),
-            y1=(n.p.y + 0.5),
-            line=dict(color="RoyalBlue", width=2),
-        )
-
-        for nb in n.neighbours:
             fig.add_shape(
-                type="line",
-                x0=(n.p.x),
-                y0=(n.p.y),
-                x1=(nb.x),
-                y1=(nb.y),
-                line=dict(color="Red", width=2),
+                type="rect",
+                x0=(n.p.x - 0.5),
+                y0=(n.p.y - 0.5),
+                x1=(n.p.x + 0.5),
+                y1=(n.p.y + 0.5),
+                line=dict(color="RoyalBlue", width=2),
             )
 
-    fig.update_yaxes(scaleanchor="x", scaleratio=1, range=[0, h])
-    fig.update_xaxes(scaleratio=1, range=[0, w])
+            for nb in n.neighbours:
+                fig.add_shape(
+                    type="line",
+                    x0=(n.p.x),
+                    y0=(n.p.y),
+                    x1=(nb.x),
+                    y1=(nb.y),
+                    line=dict(color="Red", width=2),
+                )
 
-    fig.update_layout(
-        autosize=False,
-        width=800,
-        height=800,
-        showlegend=False,
-        margin=go.layout.Margin(l=25, r=25, b=25, t=25, pad=10),
-        xaxis=dict(visible=False, fixedrange=True),
-        yaxis=dict(visible=False, fixedrange=True),
-    )
-    fig.show()
+        fig.update_yaxes(scaleanchor="x", scaleratio=1, range=[0, h])
+        fig.update_xaxes(scaleratio=1, range=[0, w])
+
+        fig.update_layout(
+            autosize=False,
+            width=800,
+            height=800,
+            showlegend=False,
+            margin=go.layout.Margin(l=25, r=25, b=25, t=25, pad=10),
+            xaxis=dict(visible=False, fixedrange=True),
+            yaxis=dict(visible=False, fixedrange=True),
+        )
+        fig.show()
+    else:
+        print("hamiltonian path does not exist!")
+
     return 0
 
 
